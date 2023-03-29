64826b5b57187f06a69c7053a89a1a409f1a849c
paolavallejo <pvallej3@eafit.edu.co>
2023-03-29 18:04:14 -0500

    new changes

---
+++ movie/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    #return HttpResponse('<h1>Welcome to Home Page</h1>')
+    #return render(request, 'home.html')
+    return render(request, 'home.html', {'name':'Paola Vallejo'})
+
+def about(request):
+    return render(request, 'about.html')
+
