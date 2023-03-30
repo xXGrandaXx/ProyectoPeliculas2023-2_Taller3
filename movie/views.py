175c290b01d9c5e2df0f4f631b894f81faa9babb
pvallej3 <pvallej3@eafit.edu.co>
2023-03-29 22:19:02 -0500

    Search form

--- movie/views.py
+++ movie/views.py
@@ -4,11 +4,12 @@ from django.http import HttpResponse
 from .models import Movie
 
 def home(request):
-    #return HttpResponse('<h1>Welcome to Home Page</h1>')
-    #return render(request, 'home.html')
-    #return render(request, 'home.html', {'name':'Paola Vallejo'})
-    movies = Movie.objects.all()
-    return render(request, 'home.html', {'movies': movies})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm: 
+       movies = Movie.objects.filter(title__icontains=searchTerm) 
+    else: 
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies})
 
 
 def about(request):
