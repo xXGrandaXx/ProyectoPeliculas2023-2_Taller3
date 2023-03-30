86f1924c83eaa5ddb1bb61bda53f0866aa5ae45a
pvallej3 <pvallej3@eafit.edu.co>
2023-03-29 21:50:35 -0500

    Movie listing

--- movie/views.py
+++ movie/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+from .models import Movie
 
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name':'Paola Vallejo'})
+    #return render(request, 'home.html', {'name':'Paola Vallejo'})
+    movies = Movie.objects.all()
+    return render(request, 'home.html', {'movies': movies})
+
 
 def about(request):
     return render(request, 'about.html')
