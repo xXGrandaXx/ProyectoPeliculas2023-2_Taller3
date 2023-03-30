d84cf176836f8424d097778907de6df08734046e
pvallej3 <pvallej3@eafit.edu.co>
2023-03-30 01:11:37 -0500

    adding navbar

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'moviereviews.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'moviereviews/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+ os.path.join(BASE_DIR, "static"),
+ 'moviereviews/static/',
+]
