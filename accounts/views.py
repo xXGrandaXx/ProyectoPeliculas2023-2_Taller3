939edb51151fb5dde577e4a6ce55a0b829c3b46e
pvallej3 <pvallej3@eafit.edu.co>
2023-03-30 01:23:29 -0500

    adding signup

---
+++ accounts/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+def signupaccount(request):
+    return render(request, 'signupaccount.html',
+    {'form':UserCreationForm})
