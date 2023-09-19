27b5d7dd14ec75b557403f34aadbac9dcccbd863
jdmartinev <110254820+jdmartinev@users.noreply.github.com>
2023-09-19 16:32:46 -0500

    Update models.py

--- DjangoProjectBase/movie/models.py
+++ DjangoProjectBase/movie/models.py
@@ -7,6 +7,9 @@ class Movie(models.Model):
  description = models.CharField(max_length=250)
  image = models.ImageField(upload_to='movie/images/')
  url = models.URLField(blank=True)
+  
+ def __str__(self):
+    return self.title
 
 class Review(models.Model):
  text = models.CharField(max_length=100)
