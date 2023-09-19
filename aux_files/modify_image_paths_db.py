4db74788bcaf4c151edccac99a3a023aa2e82e26
jdmartinev <110254820+jdmartinev@users.noreply.github.com>
2023-09-19 17:45:55 -0500

    Update modify_image_paths_db.py

--- aux_files/modify_image_paths_db.py
+++ aux_files/modify_image_paths_db.py
@@ -1,24 +1,12 @@
 from django.core.management.base import BaseCommand
 from movie.models import Movie
 import json
-import os
-import openai
-import requests
-from PIL import Image
-from io import BytesIO
+
 
 from dotenv import load_dotenv, find_dotenv
 
 _ = load_dotenv('../openAI.env')
 openai.api_key  = os.environ['openAI_api_key']
-
-def fetch_image(url):
-    response = requests.get(url)
-    response.raise_for_status()
-
-    # Convert the response content into a PIL Image
-    image = Image.open(BytesIO(response.content))
-    return(image)
 
 class Command(BaseCommand):
     help = 'Modify path of images'
@@ -29,5 +17,5 @@ class Command(BaseCommand):
             item.image.name = f"{item.image.name[0:13]}{item.title}.jpg" 
             item.save()
         
-        #self.stdout.write(self.style.SUCCESS(f'Successfully updated item with ID {item_id}'))
+        self.stdout.write(self.style.SUCCESS(f'Successfully updated with the illustrations of the movies'))
         
