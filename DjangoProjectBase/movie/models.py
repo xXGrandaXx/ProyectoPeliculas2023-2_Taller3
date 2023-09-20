5f4a91f66eec3020b7dfce367de65743bb98900d
jdmartinev <110254820+jdmartinev@users.noreply.github.com>
2023-09-19 20:19:08 -0500

    Update models.py

--- DjangoProjectBase/movie/models.py
+++ DjangoProjectBase/movie/models.py
@@ -3,20 +3,20 @@ from django.contrib.auth.models import User
 
 
 class Movie(models.Model):
- title = models.CharField(max_length=100)
- description = models.CharField(max_length=1000)
- image = models.ImageField(upload_to='movie/images/')
- url = models.URLField(blank=True)
+  title = models.CharField(max_length=100)
+  description = models.CharField(max_length=1000)
+  image = models.ImageField(upload_to='movie/images/')
+  url = models.URLField(blank=True)
   
- def __str__(self):
+  def __str__(self):
     return self.title
 
 class Review(models.Model):
- text = models.CharField(max_length=100)
- date = models.DateTimeField(auto_now_add=True)
- user = models.ForeignKey(User,on_delete=models.CASCADE)
- movie = models.ForeignKey(Movie,on_delete=models.CASCADE)
- watchAgain = models.BooleanField()
+  text = models.CharField(max_length=100)
+  date = models.DateTimeField(auto_now_add=True)
+  user = models.ForeignKey(User,on_delete=models.CASCADE)
+  movie = models.ForeignKey(Movie,on_delete=models.CASCADE)
+  watchAgain = models.BooleanField()
  
- def __str__(self):
+  def __str__(self):
     return self.text
